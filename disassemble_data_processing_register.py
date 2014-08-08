bb6a8ba7f51d9972395793671b3ae39edcae9103
Akshay Jain <uniquejainakshay@gmail.com>
2014-08-08 22:03:02 +0530

    modification

--- disassemble_data_processing_register.py
+++ disassemble_data_processing_register.py
@@ -13,6 +13,16 @@
 #MOV w1, w2
 instructions = ['ADD' ,'ADDS' ,'SUB' ,'SUBS' ,'ASR' ,'AND' ,'CMP' ,'LSL' ,'LSR' ,'MOV']
 inst_mask = ['00000000000000000000011111111110']
+inst_identifier = ['00000000000000000000010011010000']
+
 def interpret(opcode):
-	pass
+
+	for i in range(len(inst_mask)):
+		masked_opcode = int(opcode, base=2) & int(inst_mask[i], base =2)
+		print opcode , "\n", inst_mask[i], "\n", inst_identifier[i]
+		if masked_opcode == int(inst_identifier[i], base=2):
+			print "Instruction is : " , instructions[i]
+
+
+
 
